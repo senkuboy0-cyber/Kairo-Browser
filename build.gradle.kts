1b2b7d5978e086a10fcd252ea275bc2ba96312c2
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-09 17:33:27 +0600

    Align Kotlin compiler with dependency metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
